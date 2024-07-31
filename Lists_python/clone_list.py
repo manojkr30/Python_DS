40ec007977d4388b8b73ed7d47112a23b7cb53b9
manojkr30 <manojramesh3001@gmail.com>
2024-07-31 11:57:29 +0530

    [Manoj KR] Add :clone or copy a lis

---
+++ Lists_python/clone_list.py
@@ -0,0 +1,15 @@
+''''
+@Author: Manoj KR
+
+@Date: 2024-07-29 12:30:09
+
+@Last Modified by: Manoj KR
+
+@Last Modified time: 2024-07-25 12:39:03
+
+@Titl: Python program to clone or copy a list.
+'''
+if __name__=="__main__":
+   list=[7,8,9,41,10]
+   list1=list.copy()
+   print(list1)
