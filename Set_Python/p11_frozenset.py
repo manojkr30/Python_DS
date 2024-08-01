b6cc6d0deb6cbff5bd882857530fa0279653eef7
manojkr30 <manojramesh3001@gmail.com>
2024-08-01 22:46:14 +0530

    [Manoj] Add: to create an frozenset

---
+++ Set_Python/p11_frozenset.py
@@ -0,0 +1,38 @@
+"""
+@Author: Manoj KR
+
+@Date: 2024-07-29 16:58:02
+
+@Last Modified by: Manoj KR
+
+@Last Modified time: 2024-07-29 17:05:11
+
+@Title : Program to create an frozenset.
+
+"""
+
+def frozen_set():
+
+    """
+
+    description:
+        This function is used create an frozenset.
+    
+    parameters:
+        None
+        
+    return:
+        none
+
+    """
+    frozen_set = frozenset({1, 20, 3,3, 4, 5})
+    print(frozen_set)
+          
+
+def main():
+
+    frozen_set()
+
+
+if __name__ == "__main__":
+    main()
