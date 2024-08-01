9f6efcc1106b422c70752d91e7ebc63f75810768
manojkr30 <manojramesh3001@gmail.com>
2024-08-01 23:30:49 +0530

    [Manoj] Add: display formatted text (width=50) as output

---
+++ Strings_Python/p9_formatedString.py
@@ -0,0 +1,38 @@
+"""
+@Author: Manoj KR
+
+@Date: 2024-07-30 15:52:12
+
+@Last Modified by: Manoj KR
+
+@Last Modified time: 2024-07-30 16:03:11
+
+@Title : Program to display formatted text (width=50) as output.
+
+"""
+
+
+def format_string():
+
+    """
+
+    description:
+        This function is used to display formatted text (width=50) as output.
+    
+    parameters:
+        None
+        
+    return:
+        none
+
+    """
+    width = 50
+    print(f'width = {width}') 
+
+def main():
+
+    format_string()
+
+
+if __name__ == "__main__":
+    main()
