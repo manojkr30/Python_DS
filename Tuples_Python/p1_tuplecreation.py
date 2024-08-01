1ab535d52cabe5aee9a1ae72d5102429809fae42
manojkr30 <manojramesh3001@gmail.com>
2024-08-01 22:59:48 +0530

    [Manoj] Add: to create a tuple

---
+++ Tuples_Python/p1_tuplecreation.py
@@ -0,0 +1,37 @@
+"""
+@Author: Manoj KR
+
+@Date: 2024-07-29 17:08:02
+
+@Last Modified by: Manoj KR
+
+@Last Modified time: 2024-07-29 17:18:11
+
+@Title : Program to create a tuple.
+
+"""
+
+def create_tuple(tuple_of_numbers):
+
+    """
+
+    description:
+        This function is used to create tuple.
+    
+    parameters:
+        tuple_of_numbers(tuple)) : numbers in tuple we are printing.
+        
+    return:
+        none
+
+    """
+
+    print(tuple_of_numbers)
+
+def main():
+    tuple_of_numbers = (1, 2, 3, 4)
+    create_tuple(tuple_of_numbers)
+
+
+if __name__ == "__main__":
+    main()
