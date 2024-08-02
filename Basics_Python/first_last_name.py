f8167d7f52dcf51b6cdbed3d3dfd75e225d1f23f
manojkr30 <manojramesh3001@gmail.com>
2024-08-02 09:57:40 +0530

    [Manoj] Add:accepts the user's first and last name and print them in reverse order with a space between them

---
+++ Basics_Python/first_last_name.py
@@ -0,0 +1,21 @@
+'''
+@Author: Manoj KR
+
+@Date: 2024-07-27 14:38:00
+
+@Last Modified by: Manoj KR
+
+@Last Modified time: 2024-07-27 14:40:30
+
+@Title : accepts the user's first and last name and print them in
+         reverse order with a space between them.
+
+'''
+def main():
+   
+    first_name=input("Enter your First Name :")
+    last_name=input("Enter your Last Name :")
+    print(last_name,first_name)
+
+if __name__=="__main__":
+    main()
