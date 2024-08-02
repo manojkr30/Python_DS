716c1a5daf4db6cc662b49acf8e88929cb243ff2
manojkr30 <manojramesh3001@gmail.com>
2024-08-02 09:51:42 +0530

    [Manoj] Add:@Title : to display the first and last colors from the following list

---
+++ Basics_Python/display_first&lastin_list.py
@@ -0,0 +1,21 @@
+'''
+@Author: Manoj KR
+
+@Date: 2024-07-27 15:00:10
+
+@Last Modified by: Manoj KR
+
+@Last Modified time: 2024-07-27 15:04:10
+
+@Title : to display the first and last colors from the following list.
+         color_list = ["Red","Green","White" ,"Black"]
+
+'''
+def main():
+    color_list = ["Red","Green","White" ,"Black"]
+    print("first color in the list =",color_list[0] )
+    print("last color in the list=",color_list[len(color_list)-1] )
+
+if __name__=="__main__":
+    main()
+    
